1c5d2155362ddb38fd9d4b5744f7a0a2327d4c6d
SUBIKSHA333 <subikshaganesan090@gmail.com>
2026-03-04 18:51:02 +0530

    Changes

---
+++ 1.py
@@ -0,0 +1,14 @@
+print("welcome")
+username = "Subiksha"
+age = 21
+is_active = True
+value = 3.00
+print(type(username))
+print(type(age))
+print(type(is_active))
+print(type(value))
+ 
+print(username) 
+print(age)
+print(is_active)
+print(value)
